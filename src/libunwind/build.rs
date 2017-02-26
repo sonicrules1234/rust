80ee0bb7b5b8f86087b4ab3fea7e1348a5f730ac
Samuel Holland <samuel@sholland.org>
2017-02-25 23:51:58 -0600

    Link libgcc_s over libunwind on musl

--- src/libunwind/build.rs
+++ src/libunwind/build.rs
@@ -14,12 +14,8 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     let target = env::var("TARGET").expect("TARGET was not set");
 
-    if target.contains("linux") {
-        if target.contains("musl") && !target.contains("mips") {
-            println!("cargo:rustc-link-lib=static=unwind");
-        } else if !target.contains("android") {
-            println!("cargo:rustc-link-lib=gcc_s");
-        }
+    if target.contains("linux") && !target.contains("android") {
+        println!("cargo:rustc-link-lib=gcc_s");
     } else if target.contains("freebsd") {
         println!("cargo:rustc-link-lib=gcc_s");
     } else if target.contains("rumprun") {
