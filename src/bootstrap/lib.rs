2dca67022c550b8ea69623fcaa04bee10112ea77
Samuel Holland <samuel@sholland.org>
2017-09-08 00:04:29 -0500

    Fix LLVM build

--- src/bootstrap/lib.rs
+++ src/bootstrap/lib.rs
@@ -642,7 +642,8 @@ impl Build {
         // cc-rs because the build scripts will determine that for themselves.
         let mut base = self.cc[&target].args().iter()
                            .map(|s| s.to_string_lossy().into_owned())
-                           .filter(|s| !s.starts_with("-O") && !s.starts_with("/O"))
+                           .filter(|s| !s.starts_with("-O") && !s.starts_with("/O")
+                                   && !s.starts_with("-static"))
                            .collect::<Vec<_>>();
 
         // If we're compiling on macOS then we add a few unconditional flags
