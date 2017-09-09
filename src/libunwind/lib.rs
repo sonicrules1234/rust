a082239a62524f1428e7e41faf75715d7c8bab3d
Samuel Holland <samuel@sholland.org>
2017-09-09 00:14:16 -0500

    Prefer libgcc_eh over libunwind for musl

--- src/libunwind/lib.rs
+++ src/libunwind/lib.rs
@@ -36,6 +36,6 @@ cfg_if! {
 }
 
 #[cfg(target_env = "musl")]
-#[link(name = "unwind", kind = "static", cfg(target_feature = "crt-static"))]
+#[link(name = "gcc_eh", cfg(target_feature = "crt-static"))]
 #[link(name = "gcc_s", cfg(not(target_feature = "crt-static")))]
 extern {}
