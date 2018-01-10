ba948554d0241f52fdc762abf21c101445d1fdc4
Samuel Holland <samuel@sholland.org>
2018-01-10 13:36:41 -0600

    Don't pass CFLAGS to the C++ compiler

--- src/bootstrap/builder.rs
+++ src/bootstrap/builder.rs
@@ -1244,8 +1244,7 @@ impl<'a> Builder<'a> {
             if let Ok(cxx) = self.cxx(target) {
                 let cxx = ccacheify(&cxx);
                 cargo
-                    .env(format!("CXX_{}", target.triple), &cxx)
-                    .env(format!("CXXFLAGS_{}", target.triple), cflags);
+                    .env(format!("CXX_{}", target.triple), &cxx);
             }
         }
 
