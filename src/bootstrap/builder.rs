d1cc6048465f4cc7a06495b424693aa0c27f3137
Samuel Holland <samuel@sholland.org>
2018-01-10 13:36:41 -0600

    Don't pass CFLAGS to the C++ compiler

--- src/bootstrap/builder.rs
+++ src/bootstrap/builder.rs
@@ -689,7 +689,7 @@ impl<'a> Builder<'a> {
 
             let cflags = self.cflags(target).join(" ");
             cargo.env(format!("CFLAGS_{}", target), cflags.clone())
-                 .env("CFLAGS", cflags.clone());
+                 .env("CFLAGS", cflags);
 
             if let Some(ar) = self.ar(target) {
                 let ranlib = format!("{} s", ar.display());
@@ -701,9 +701,7 @@ impl<'a> Builder<'a> {
 
             if let Ok(cxx) = self.cxx(target) {
                 cargo.env(format!("CXX_{}", target), cxx)
-                     .env("CXX", cxx)
-                     .env(format!("CXXFLAGS_{}", target), cflags.clone())
-                     .env("CXXFLAGS", cflags);
+                     .env("CXX", cxx);
             }
         }
 
