fba86e563a20ed4569f679c40686e05ccb619d93
Samuel Holland <samuel@sholland.org>
2018-08-07 21:59:15 -0500

    Add powerpc64-unknown-linux-musl target

--- src/tools/build-manifest/src/main.rs
+++ src/tools/build-manifest/src/main.rs
@@ -91,6 +91,7 @@ static TARGETS: &'static [&'static str] = &[
     "powerpc-unknown-linux-gnuspe",
     "powerpc-unknown-linux-musl",
     "powerpc64-unknown-linux-gnu",
+    "powerpc64-unknown-linux-musl",
     "powerpc64le-unknown-linux-gnu",
     "powerpc64le-unknown-linux-musl",
     "riscv32imc-unknown-none-elf",
