e9b3fad02ce6e318462fb413d3834cdeccb47831
Samuel Holland <samuel@sholland.org>
2018-09-10 01:36:00 +0000

    Use python3 instead of python
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- x.py
+++ x.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2016 The Rust Project Developers. See the COPYRIGHT
 # file at the top-level directory of this distribution and at
 # http://rust-lang.org/COPYRIGHT.
